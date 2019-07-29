7c73d7965856f00a6e82d414844d4d50bd40ee43
thejamilah <jamilahmcw@gmail.com>
2019-07-29 09:23:45 -0700

    Ruby Palindrome

--- 01-Problem-Domain/01-Palindrome/palindrome-00.rb
+++ 01-Problem-Domain/01-Palindrome/palindrome-00.rb
@@ -2,3 +2,44 @@
 Problem Domain
 Create a method `palindrome?()` that takes in a string as an argument and determines whether that string is a palindrome. Output the input string with a return of `True` or `False` for the method.
 =end
+=begin
+def palindrome?(string)
+  string == string.reverse
+end
+
+def palindrome?(string)
+  string.downcase == string.downcase.reverse
+end
+
+def palindrome?(string)
+  #string.downcase.delete(' ') == string.downcase.delete(' ').reverse
+  #refactor for DRY
+  str = string.downcase.delete(' ')
+  str == str.reverse
+end
+=end
+
+def switch(string)
+  forward = string.split('')
+  reverse = []
+  counter = -1
+    until reverse.length == forward.length
+      reverse << forward[counter]
+      counter -= 1
+    end
+    reverse.join
+end
+
+
+
+def palindrome?(string)
+  switch(string).downcase == string.downcase
+end
+
+# puts switch("hana")
+puts ("Is \" \" a Palindrome? #{palindrome?(" ")}")
+puts ("Is \"rr\" a Palindrome? #{palindrome?("rr")}")
+puts ("Is \"hanah\" a Palindrome? #{palindrome?("hanah")}")
+puts ("Is \"flatiron\" a Palindrome? #{palindrome?("flatiron")}")
+puts ("Is \"hell0\" a Palindrome? #{palindrome?("hello")}")
+puts ("Is \"Tacocat\" a Palindrome? #{palindrome?("Tacocat")}")
--- 01-Problem-Domain/01-Palindrome/palindrome-03.rb
+++ 01-Problem-Domain/01-Palindrome/palindrome-03.rb
@@ -1,3 +1,8 @@
+=begin
+Problem Domain
+Create a method `palindrome?()` that takes in a string as an argument and determines whether that string is a palindrome. Output the input string with a return of `True` or `False` for the method.
+=end
+
 # Solution 3
 
 def palindrome?(string)
@@ -9,8 +14,9 @@ def palindrome?(string)
   true
 end
 
-puts palindrome?("")
-puts palindrome?("rr")
-puts palindrome?("hanah")
-puts palindrome?("flatiron")
-puts palindrome?("hannah")
+puts ("Is \" \" a Palindrome? #{palindrome?(" ")}")
+puts ("Is \"rr\" a Palindrome? #{palindrome?("rr")}")
+puts ("Is \"hanah\" a Palindrome? #{palindrome?("hanah")}")
+puts ("Is \"flatiron\" a Palindrome? #{palindrome?("flatiron")}")
+puts ("Is \"hell0\" a Palindrome? #{palindrome?("hello")}")
+puts ("Is \"Tacocat\" a Palindrome? #{palindrome?("Tacocat")}")
